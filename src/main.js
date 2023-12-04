fe05527a32156c9445e3997ced844f09b2176a57
rifkyhilman <rifkyhilman265@gmail.com>
2023-12-04 23:05:47 +0700

    :wrench: New setup use prisma and postgresql
    new setup use prisma and postgresql

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
+
 const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send("Hallo dek cantik!");
+    res.send("Hello World !");
 });
 
 app.listen(PORT, () => {
