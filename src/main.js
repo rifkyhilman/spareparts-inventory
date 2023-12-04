281dc32ec5dd70292b0e74194d30561802345865
rifkyhilman <rifkyhilman265@gmail.com>
2023-12-05 04:55:59 +0700

    :construction: Add create data product
    add create data product

--- src/main.js
+++ src/main.js
@@ -8,16 +8,35 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello World !!")
 });
 
-app.get('/products',async (req, res) => {
+app.get('/products', async(req, res) => {
     const products = await prisma.product.findMany();
 
     res.send(products);
 });
 
+app.post('/products', async(req, res) => {
+   const newDataProducts = req.body;
+   
+    const product = await prisma.product.create({
+        data: {
+            name: newDataProducts.name,
+            description: newDataProducts.description,
+            price: newDataProducts.price,
+            image: newDataProducts.image            
+        },
+    });
+
+    res.send({
+        message: "Create Product Success",
+        data: product
+    })
+});
 
 app.listen(PORT, ()=>{
     console.log(`server is runing at port : ${PORT}`)
