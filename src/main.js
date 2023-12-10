657c8251defc998d4a3c29ff3399d2af97a03a67
rifkyhilman <rifkyhilman265@gmail.com>
2023-12-10 23:08:54 +0700

    :lipstick: Add view engine EJS
    add view engine EJS

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
-
+const path = require('path');
 const express = require("express");
 
 const app = express();
 const productController = require("./product/product.controller");
+const routes = require("./routes");
 
 const PORT = process.env.PORT;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.json());
 app.use('/products', productController);
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`server is runing at port : ${PORT}`)
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    res.render('Home.ejs', {
+        nama: "Rifki"
+    });
+});
+
+module.exports = router;
