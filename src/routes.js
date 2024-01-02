61c13febbc502eef75d167c914c2963f125635d9
rifkyhilman <rifkyhilman265@gmail.com>
2024-01-03 03:17:36 +0700

    :construction: Add bootstrap icons
    add bootstrap icons

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 
 router.get('/', (req, res) => {
-    res.render('home.ejs')
+    res.render('home.ejs', {
+        title: "Home Page"
+    })
 });
 
 module.exports = router;
