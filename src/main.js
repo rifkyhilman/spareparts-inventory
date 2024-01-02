1d01d32dcca6973717646ac95acc6e60cedff5ac
rifkyhilman <rifkyhilman265@gmail.com>
2024-01-03 02:51:27 +0700

    :construction: Setup bootstrap use NPM
    setup bootstrap use NPM

--- src/main.js
+++ src/main.js
@@ -1,13 +1,46 @@
+// imports
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const path = require('path');
 
 const app = express();
-const productController = require("./product/product.controller");
+const PORT = process.env.PORT || 4000;
 
-const PORT = process.env.PORT;
-
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use('/products', productController);
+
+// use bootstrap
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')))
+
+// set tempalate engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// route prefix
+app.use('/', require("./routes"));
+app.use('/products', require("./product/product.controller"));  
+
+
+
+
+
+// middlewares
+app.use(
+  session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
 
 app.listen(PORT, () => {
     console.log(`server is runing at port : ${PORT}`)
